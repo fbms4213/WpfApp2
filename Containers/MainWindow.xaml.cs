af588a7d2733f791728a827428f7e6a92cd1053e
Tural Novruzov <t.novruzov@itstep.edu.az>
2022-10-20 10:45:09 +0400

    Add project files.

---
+++ Containers/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Containers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //stackPanel.Children.Add(new Button()
+            //{
+                //Content = "4",
+                //FontSize = 42,
+                //Width = 100
+            //});
+        }
+
+
+        private void Button_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                MessageBox.Show("LeftButton");
+            else
+                MessageBox.Show("RightButton");
+        }
+    }
+}
